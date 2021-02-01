d9b0ca589954a2609f3f4375b550c07b4cf91ed2
Zach Sabin <zacharysabin@gmail.com>
2021-02-01 11:53:15 -0800

    filter out IR

--- your_league/src/scraper.js
+++ your_league/src/scraper.js
@@ -5,6 +5,7 @@ for (let teamTable of teamTables) {
     let playerTable = teamTable.children[1].firstElementChild.children[1].firstElementChild.children[4]
     for (let player of playerTable.children) {
         if (player.children[1].firstElementChild.firstElementChild.children.length < 2) continue;
+        if (player.firstElementChild.firstElementChild.textContent == "IR") continue;
         let playerName = player.children[1].firstElementChild.firstElementChild.children[1].firstElementChild.firstElementChild.firstElementChild.textContent
         playerTeams[playerName] = teamName
     }
