6c0f0e5e5acf5c8855cc34b10eb1ba12c085e277
Zach Sabin <zacharysabin@gmail.com>
2021-05-07 16:54:19 -0700

    cleanout old directory

--- league_analysis/scraper.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName('ResponsiveTable')[0].children[0].firstElementChild.lastElementChild.title
-
-let name = document.getElementsByClassName('ResponsiveTable')[0].children[1].firstElementChild.children[1].firstElementChild.lastElementChild.children[0].children[1].firstElementChild.firstElementChild.firstElementChild.firstElementChild.text
-
-let cost = 
-document.getElementsByClassName('ResponsiveTable')[0].children[1].firstElementChild.children[1].firstElementChild.lastElementChild.children[0].children[2].firstElementChild.firstElementChild.textContent.replace(/\$/g, '')
